54db80b7b23287e8bc5f3029f32ad4dc72fc14ec
BarakinSergey <55586597+BarakinSergey@users.noreply.github.com>
2019-11-13 17:10:21 +0300

    Add files via upload

--- play.py
+++ play.py
@@ -46,15 +46,20 @@ person = []
 result = []
 name = 0
 score = 0
+scorelast = score
+timelast = 0
 for i in range(1,12+1):
 	person.append(0)
 	result.append(0)
 file = open('table.txt', 'a')
 file.close()
 
-global phase
 phase = 0
-
+gone_menu = 0
+gone_active = 0
+gone_playing_start = 0
+gone_playing_end = 0
+gone_resulttable = 0
 #---------------------------------------------------------------------------------------------------
 #classes and functions
 class play:	#new_ball and move_ball - creating or changing and moving certain
@@ -65,8 +70,8 @@ class play:	#new_ball and move_ball - creating or changing and moving certain
 		y = rnd(100,400)
 		ysit=y
 		r = rnd(20,30)
-		dx = rnd(-200,200)/50
-		dy = rnd(-200,200)/50
+		dx = rnd(-100,100)/50
+		dy = rnd(-100,100)/50
 		obj=canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0, tag =  'ball' + str(num))
 		if pose==1:
 			xlist.insert(num,x)
@@ -112,19 +117,20 @@ class interaction:
 		#print(xp,yp,(xp-xsitlist[num])*(xp-xsitlist[num])+(yp-ysitlist[num])*(yp-ysitlist[num]),rlist[num]*rlist[num])
 		if (xp-xsitlist[num])*(xp-xsitlist[num])+(yp-ysitlist[num])*(yp-ysitlist[num])<=rlist[num]*rlist[num]:
 			score=score+1
-			print(score)
+			#print(score)
 			canv.delete('ball' + str(num))
 			pl_ay.new_ball(num,0)
 			canv.create_rectangle(870, 190, 930, 210, fill='yellow', outline='green', width=3)
 			canv.create_text(900, 200, text=score,
                 justify=CENTER, font="Verdana 14")
+	
 	def nameenter(self, event):
-	    global name
-	    s = e.get()
-	    s = s.split()
-	    s.sort()
-	    name = ' '.join(s)
-	    root.after(5,resulttable)
+		global name
+		s = e.get()
+		s = s.split()
+		#s.sort()
+		name = ' '.join(s)
+		root.after(5,resulttable)
 inter_action=interaction()
 
 def create(): #creates quan number of balls #and count number of squares
@@ -133,32 +139,41 @@ def create(): #creates quan number of balls #and count number of squares
 		pl_ay.new_ball(point,1)
 		#print("new_ball",point)
 
-def clickact(num): #checks if user pointed in the area of nummed button
+def clickact(): #checks if user pointed in the area of nummed button
 	global score
 	global phase
 	global t0
+	global gone_active
+	global gone_playing_start
 	if (250<xplast) and (xplast<750) and (170<yplast) and (yplast<220):
 		phase = 1
-		canv.create_rectangle(0, 0, 1000, 600, fill='white')
-		create()
-		canv.create_rectangle(800, 0, 1000, 600, fill='yellow', outline='white',
-	                    width=3)
-		canv.create_text(900, 100, text="Try to get as much\n as you can \n for the next \n 60 sec",
-		                justify=CENTER, font="Verdana 14")
-		canv.create_text(900, 170, text="YOUR SCORE:",
-		                justify=CENTER, font="Verdana 14")
-		canv.create_rectangle(870, 190, 930, 210, fill='yellow', outline='green',
+		if gone_playing_start == 0:
+			canv.create_rectangle(0, 0, 1000, 600, fill='white')
+			create()
+			canv.create_rectangle(800, 0, 1000, 600, fill='yellow', outline='white',
 		                    width=3)
-		canv.create_text(900, 200, text=score,
+			canv.create_text(900, 100, text="Try to get as much\n as you can \n for the next \n 60 sec",
+			                justify=CENTER, font="Verdana 14")
+			canv.create_text(900, 170, text="YOUR SCORE:",
+			                justify=CENTER, font="Verdana 14")
+			canv.create_rectangle(870, 190, 930, 210, fill='yellow', outline='green',
+			                    width=3)
+			canv.create_text(900, 200, text=score,
+			                justify=CENTER, font="Verdana 14")
+			canv.create_text(900, 230, text="PASSED TIME:",
+			                justify=CENTER, font="Verdana 14")
+			canv.create_text(955, 260, text="sec",
+			                justify=CENTER, font="Verdana 14")
+			canv.create_rectangle(870, 250, 930, 270, fill='yellow', outline='green',
+		                    width=3)
+			t0 = time.time()
+			canv.create_text(900, 260, text=0,
 		                justify=CENTER, font="Verdana 14")
-		canv.create_text(900, 230, text="PASSED TIME:",
-		                justify=CENTER, font="Verdana 14")
-		canv.create_text(955, 260, text="sec",
-		                justify=CENTER, font="Verdana 14")
-		t0 = time.time()
+		gone_playing_start = 1
 		playing()
 	if (250<xplast) and (xplast<750) and (240<yplast) and (yplast<290):
 		phase = 2
+		
 		#reading result table
 		file = open('table.txt', 'r')
 		number = 0
@@ -180,12 +195,13 @@ def clickact(num): #checks if user pointed in the area of nummed button
                 fill="red", font="Verdana 17")
 			canv.create_text(700, 100+30*(1+i), text=str(result[i-1]),
                 fill="red", font="Verdana 17")
-		canv.create_rectangle( 420, 445, 580, 475, fill='white', outline='red', width=3)
-		canv.create_text(500, 460, text="return to menu",
-                fill="red" , justify=CENTER, font="Verdana 13")
+			canv.create_rectangle( 420, 445, 580, 475, fill='white', outline='red', width=3)
+			canv.create_text(500, 460, text="return to menu",
+	                fill="red" , justify=CENTER, font="Verdana 13")
 	if phase==2 and (420<xplast) and (xplast<580) and (445<yplast) and (yplast<475):
 		print("return to menu")
 		phase=0
+		gone_active = 0
 		active()
 		
 			
@@ -193,23 +209,28 @@ def active(): #for affecting programm
 	global xplast
 	global yplast
 	global phase
+	global gone_active
 	canv.bind('<Button-1>', inter_action.clack)
 	if phase==0:
 		#print("active")
 		#canv.create_text(500, 100, text="CATCH  	THE 	BALL\nget as much as possible for fixed time",
 	    #            fill="green" , justify=CENTER, font="Verdana 14")
-		canv.create_rectangle(0, 0, 1000, 600, fill='white')
-		canv.create_rectangle( 50,  50, 950, 150, fill='yellow', outline='green', width=3)
-		canv.create_text(500, 100, text="CATCH  	THE 	BALL\nget as much as possible for fixed time",
-	                fill="green" , justify=CENTER, font="Verdana 14")
-		canv.create_rectangle(250, 170, 750, 220, fill='yellow', outline='green', width=3)
-		canv.create_text(500, 195, text="PLAY",
-	                fill="green" , justify=CENTER, font="Verdana 14")
-		canv.create_rectangle(250, 240, 750, 290, fill='yellow', outline='green', width=3)
-		canv.create_text(500, 265, text="RESULTS",
+		if gone_active == 0:
+			canv.create_rectangle(0, 0, 1000, 600, fill='white')
+			canv.create_rectangle( 50,  50, 950, 150, fill='yellow', outline='green', width=3)
+			canv.create_text(500, 100, text="CATCH  	THE 	BALL\nget as much as possible for fixed time",
 		                fill="green" , justify=CENTER, font="Verdana 14")
+			canv.create_rectangle(250, 170, 750, 220, fill='yellow', outline='green', width=3)
+			canv.create_text(500, 195, text="PLAY",
+		                fill="green" , justify=CENTER, font="Verdana 14")
+			canv.create_rectangle(250, 240, 750, 290, fill='yellow', outline='green', width=3)
+			canv.create_text(500, 265, text="RESULTS",
+			                fill="green" , justify=CENTER, font="Verdana 14")
+			print("font active")
+			gone_active = 1
+
 	if (xplast-xp)*(yplast-yp)!=0:
-		clickact(1)
+		clickact()
 		print("clickact")
 	xplast=xp
 	yplast=yp
@@ -221,6 +242,9 @@ def playing(): #realises the game
 	global t0
 	global name
 	global phase
+	global gone_playing_end
+	global gone_resulttable
+	global timelast
 	canv.bind('<Button-1>', inter_action.clack)
 	if phase==1:
 		for point in range(1, quan+1):
@@ -228,27 +252,32 @@ def playing(): #realises the game
 			#print("move_ball",point)
 			if (xplast-xp)*(yplast-yp)!=0:
 				inter_action.click(point)
-				print("click",point)
+				#print("click",point)
 		xplast=xp
 		yplast=yp
 		tnow=int(time.time()-t0)
-		canv.create_rectangle(870, 250, 930, 270, fill='yellow', outline='green',
-	                    width=3)
-		canv.create_text(900, 260, text=tnow,
-	                justify=CENTER, font="Verdana 14")
+		if tnow != timelast:
+			canv.create_rectangle(870, 250, 930, 270, fill='yellow', outline='green',
+		                    width=3)
+			canv.create_text(900, 260, text=tnow,
+		                justify=CENTER, font="Verdana 14")
+			timelast =tnow
 		if tnow <= 0*24+1*60+0*1-1: #-----------------------------------------------------------------------------TIMER----------------------
 			root.after(1,playing)
 		else:
 			phase=1.5
-			canv.create_rectangle(150, 150, 850, 350, fill='yellow')
-			canv.create_text(500, 200, text="That's over",
-	                justify=CENTER, font="Verdana 30", fill="black")
-			canv.create_text(500, 240, text="Print your nickname to save results, \n after that push enter\n instead of space between words use underline",
-	                justify=CENTER, font="Verdana 12", fill="black")
-			e.place(x=450, y=270, width=100, height=30)
-			b.place(x=450, y=310, width=100, height=30)
+			if gone_playing_end == 0:
+				canv.create_rectangle(150, 150, 850, 350, fill='yellow')
+				canv.create_text(500, 190, text="That's over",
+		                justify=CENTER, font="Verdana 30", fill="black")
+				canv.create_text(500, 240, text="Print your nickname to save results, \n after that push enter\n instead of space between words use underline",
+		                justify=CENTER, font="Verdana 12", fill="black")
+				e.place(x=450, y=270, width=100, height=30)
+				b.place(x=450, y=310, width=100, height=30)
+				gone_playing_end = 1
 			print("resulttable")
 			phase = 2
+			gone_resulttable = 0
 			root.after(1, resulttable)
 
 def resulttable(): #entering score in table
@@ -257,6 +286,7 @@ def resulttable(): #entering score in table
 	global person
 	global result
 	global phase
+	global gone_resulttable
 	print(name)
 	if name!=0:
 		name = str(name) + '\n'
@@ -307,6 +337,7 @@ def resulttable(): #entering score in table
 		name = 0
 		score = 0
 		print("phase", name, score, phase)
+		gone_active = 0
 		active()
 
 	b.bind('<Button-1>', inter_action.nameenter)
@@ -314,7 +345,7 @@ def resulttable(): #entering score in table
 
 #---------------------------------------------------------------------------------------------------
 #main part
-active()
 print("start")
+active()
 root.after(0, active)
 mainloop()
